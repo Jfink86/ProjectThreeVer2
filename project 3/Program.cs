6e807ad50191e9eb7bed803b58b3d6f954cbebfa
jfink86 <jfink86@yahoo.com>
2018-02-06 09:27:01 -0500

    final commit

---
+++ project 3/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Project_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string action = "";
+            int ownerAction; 
+            
+            Console.WriteLine("Welcome to the VirtuaDog App");
+            Console.ReadLine();
+
+            Dog dogOwner = new Dog();
+
+            do
+            {
+                
+                dogOwner.Tick();
+                Console.WriteLine();
+
+                Console.WriteLine("<LINCOLNS CURRENT STATUS>");
+                dogOwner.Dogstatus();
+                Console.WriteLine();
+
+                Console.WriteLine("To Feed Lincoln...........: Type 1");
+                Console.WriteLine("To let Lincoln OutSide....: Type 2");
+                Console.WriteLine("To give Licoln a Treat....: Type 3");
+                Console.WriteLine("To exit.................. : Type exit ");
+
+                ownerAction = int.Parse(Console.ReadLine());
+
+                switch (ownerAction)
+                {
+
+                    case 1:
+                        dogOwner.FeedDog();
+                        break;
+
+                    case 2:
+                        dogOwner.PottyStatus();
+                        break;
+
+                    case 3:
+                        dogOwner.WantaTreat();
+                        break;
+
+                    default:
+                        break;
+
+                }
+
+                action = " ";  
+            } while (action != "exit");
+            {
+             Console.WriteLine("Thanks for using the VirtualDog app"); 
+            }
+           
+
+
+        }
+    }
+}    
+
---
+++ project 3/pet.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_3
+{
+    class Dog
+    {
+
+        //Fields
+
+        private bool treat;
+        private bool isHungry;
+        private bool goPotty;
+        private int tickercount;
+        // Properties
+        public int Tickercount
+        {get { return this.tickercount; }
+            set { this.tickercount = value; }
+
+
+        }
+
+        public bool Treat
+        {
+            get { return this.treat; }
+            set { this.treat = value; }
+        }
+
+        public bool IsHungry
+        {
+            get { return this.isHungry; }
+            set { this.isHungry = value; }
+        }
+
+        public bool GoPotty
+        {
+            get { return this.goPotty; }
+            set { this.goPotty = value; }
+        }
+
+        //  Constructors
+
+        public Dog()
+        {
+
+        }
+
+        public Dog(bool treat, bool isHungry, bool goPotty)
+        {
+            this.treat = treat;
+            this.isHungry = isHungry;
+            this.goPotty = goPotty;
+        }
+
+        // Methods
+
+        public void WantaTreat()
+        {
+            treat = true;
+            Console.WriteLine("Isa Good Boy? Isa wanna Treat?");
+        }
+
+        public void FeedDog()
+        {
+            Console.WriteLine("Get the dinner BooBoo!!!");
+            isHungry = true;
+        }
+
+        public void PottyStatus()
+        {
+            Console.WriteLine("Datta Wadda Good Boy BooBoo");
+            goPotty = true;
+        }
+
+        public void Dogstatus()
+        {
+            Console.WriteLine("Wants to Eat.........:" + isHungry);
+            Console.WriteLine("Wants to go OutSide..:" + goPotty);
+            Console.WriteLine("Wants a Treat........:" + treat);
+        }
+
+        public void Tick()
+        {
+
+            tickercount++;
+            if (isHungry == false)
+            {
+                goPotty = true;
+            }
+            if (goPotty == false )
+            {
+                treat = true;
+            }
+            if (isHungry == false)
+            {
+                isHungry = true;
+            }
+            else treat = false;
+
+
+
+
+
+
+        }
+
+
+
+
+    }    
+
+}
+
+
+    
+
